Fix err calculation

poisson.py
@@ -282,4 +282,4 @@ class Poisson2D(PDE):
             self.r = np.vstack(r_parts)
             self.d = np.vstack(d_parts)
 
-        self.err = np.linalg.norm(self.r[1:-1, 1:-1], ord=1) / self.N**2
+        self.err = np.sum(self.r[1:-1, 1:-1]) / self.N**2